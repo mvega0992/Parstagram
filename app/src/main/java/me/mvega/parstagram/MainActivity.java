1a9474bbf7461ca6e685546eb027e7400cca82f1
Miguel Vega <mvega@fb.com>
2018-07-09 23:33:03 -0700

    Styled login screen

--- app/src/main/java/me/mvega/parstagram/MainActivity.java
+++ app/src/main/java/me/mvega/parstagram/MainActivity.java
@@ -1,12 +1,15 @@
 package me.mvega.parstagram;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -17,15 +20,27 @@ public class MainActivity extends AppCompatActivity {
     private EditText usernameInput;
     private EditText passwordInput;
     private Button loginBtn;
+    TextInputLayout textInputLayout;
+
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+
         usernameInput = findViewById(R.id.username_et);
         passwordInput = findViewById(R.id.password_et);
         loginBtn = findViewById(R.id.login_btn);
+        textInputLayout = findViewById(R.id.username_text_input_layout);
+
+        textInputLayout.setHintEnabled(false);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +51,22 @@ public class MainActivity extends AppCompatActivity {
                 login(username, password);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            animationDrawable.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            animationDrawable.stop();
+        }
     }
 
     private void login(String username, String password) {
